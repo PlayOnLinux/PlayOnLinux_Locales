7614e5725d0f65edcd63dcd8bf7d9508520b056b
Quentin Pâris <git@qparis.fr>
2012-08-16 18:39:46 +0200

    rules

---
+++ includes/en/rules.php
@@ -0,0 +1,35 @@
+<h1>Charte d'utilisation</h1>
+<h2>Piratage, sujets à caractères illégaux</h1>
+<p><?php echo $website_name; ?> est <b>contre le piratage</b>. L'utilisateur demandant de l'aide à propos d'un logiciel s'engage donc à ne pas utiliser une version piratée. Il est également interdit aux membres du site de fournir ou de demander de l'aide à propos des méthodes de contournement des protections mises en place par les éditeurs de logiciels. Il en va de même à propos des copies non-officielles de support d'installation sous forme d'images CD. Dans le cas où un logiciel nécessite le contournement de la protection mise en place par l'éditeur pour fonctionner avec <?php echo $website_name; ?>, <?php echo $website_name; ?> ne pourra en aucun cas vous fournir une quelconque aide. De plus, cette démarche sera sous votre entière responsabilité. </p>
+<p>Les autres sujets à caractère illégal (tel que la pornographie, le racisme ou la diffamation) sont également interdits</p>
+
+<h2>Responsabilité</h2>
+<p>Bien que nous surveillions attentivement le contenu du site, <?php echo $website_name; ?> ne saurait être responsable en cas de dommage sur votre ordinateur lorsque-vous utilisez un contenu téléchargé sur le site internet.</p> 
+
+<h2>Contenu posté</h2>
+<p>Toute personne tentant de poster du contenu malveillant sur le site (installateurs ou programmes malveillants) s'expose à un bannissement définitif et sans préavis.</p>
+<p>En postant du contenu sur le site, vous acceptez que celui-ci soit placé sous licence GPLv3, sauf mention contraire de votre part.</p>
+<p>L'équipe de <?php echo $website_name; ?> se réserve le droit de supprimer le contenu du site internet.</p>
+
+<h2>Politesse, courtoisie</h2>
+<p><?php echo $website_name; ?> est un projet entièrement libre et gratuit. Il ne fonctionne que sur la base du bénévolat. Les membres de l'équipe acceptent d'aider les utilisateurs sur leur temps libre. Il est donc possible que vous n'obteniez pas toujours vos réponses immédiatement. Par conséquent, il est interdit de lancer un rappel ("up") sur un problème moins de 24 heures après l'envois du dernier message.</p>
+<p>Vous devez respecter <b>les règles élémentaires de politesse</b>. Un simple <i>bonjour</i> en début de sujet, et un <i>merci</i> nous donnera d'avantage l'envi de vous aider.</p>
+<p>Les insultes sont interdites sur le site.</p>
+
+<h2>Orthographe, langage</h2>
+<p>Afin de pouvoir être lisibles par tout le monde le plus rapidement possible, vous devez poster avec <b>une orthographe convenable</b>. En particulier, <b>les messages écris en SMS sont systématiquement supprimés.</b></p>
+<p>Il est impératif de respecter la langue du forum sur lequel vous vous trouvez. (Vous devez écrire en Français sur le forum Français, en Anglais sur le forum Anglais.)</p>
+
+<h2>Sujets à caractère polémiques</h2>
+<p>Il est fortement déconseillé de lancer des sujets à caractères polémiques sur le forum ("Trolls"). Les critiques non constructives envers un logiciel (ou une marque, catégorie de logiciels) sont également interdites. Par exemple, tout message comprenant les mots <i>"Windobe"</i> ou <i>"Micro$oft"</i> seront fermés.</p>
+
+<h2>Lecture de post-it</h2>
+<p>Dans chaque forum, il existe des sujets mis en évidence, marqués avec un <b>post-it</b>. Leur lecture est obligatoire avant de poster un message dans ce forum.</p>
+
+<h2>Interdiction de créer plusieurs comptes</h2>
+<p>Il est interdit à un membre de créer plusieurs comptes.</h2>
+	
+<h2>Spam, flood publicité</h2>
+<p>Le spam, le flood et la publicité sont interdites. Le site web contient un système antispam susceptible de vous bannir si vous utilisez le site pour flooder ou pour envoyer du spam.</p>
+
+<p>En cas de non-respect aux règles énoncées ci-dessus, <?php echo $website_name; ?> se réserve le droit de vous bannir de tout les serveurs. (Forum, chat, récupération d'installateurs depuis le logiciel)</p>
