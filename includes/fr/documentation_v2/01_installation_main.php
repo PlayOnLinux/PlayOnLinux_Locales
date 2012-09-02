6fa4200bf64d5bdcc0eac718b85fe86fa8f54890
tharvik <tharvik@gmail.com>
2012-09-02 20:30:01 +0200

    [fr] Documentation: add "Next chapiter"

--- includes/fr/documentation_v2/01_installation_main.php
+++ includes/fr/documentation_v2/01_installation_main.php
@@ -12,3 +12,4 @@ else
 
 <p>Voilà pour l'installation de <?php echo $website_name; ?>. Vous devez ainsi avoir une application fonctionnelle.
 Si ce n'est pas le cas, relisez bien les instructions d'installation, et si vous êtes sûr que c'est pas un problème hors de notre compétence, poster un message sur le forum, dans la section "Site Internet".</p>
+<p>Prochain chapitre : <a href="02_lancement.php">Premier lancement</a></p>
