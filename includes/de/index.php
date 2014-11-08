f5cb863fc06ae06bef07f12cef5089931cfc288b
GoWale <laengerwiekuerzer@web.de>
2014-11-08 23:54:31 +0100

    Update index.php

--- includes/de/index.php
+++ includes/de/index.php
@@ -1,67 +1,70 @@
 <h1><!--Home-->Startseite</h1>
-<div class="presentation_item" onclick="pres_show(1)"><img src="<?php echo $url; ?>/images/logos/logo48.png" alt="PlayOnLinux" /><span class="title">I'd like to learn more about PlayOnLinux</span><span class="description">What is PlayOnLinux?</span></div>
+<div class="presentation_item" onclick="pres_show(1)"><img src="<?php echo $url; ?>/images/logos/logo48.png" alt="PlayOnLinux" /><span class="title"><!--I'd like to learn more about PlayOnLinux-->Ich möchte mehr über PlayOnLinux erfahren</span><span class="description"><!--What is PlayOnLinux?-->Was ist PlayOnLinux?</span></div>
 <div id="pres_item1" class="presentation_item_hidden">
-   <h2>What is PlayOnLinux ?</h2>
-   <p>PlayOnLinux is a piece of software which allows you <b>to easily install and use</b> numerous <b>games and apps designed to run with Microsoft&reg; Windows&reg;.</b><br/>
+   <h2><!--What is PlayOnLinux ?-->Was ist PlayOnLinux?</h2>
+   <p><!--PlayOnLinux is a piece of software which allows you <b>to easily install and use</b> numerous <b>games and apps designed to run with Microsoft&reg; Windows&reg;.</b><br/>
    Few games are compatible with GNU/Linux at the moment and it certainly is a factor preventing the migration to this system. PlayOnLinux brings a cost-free, accessible and efficient solution to this problem.<br/></p>
-   <h2>What are PlayOnLinux's features?</h2>
-   <p>Here is a non-exhaustive list of the interesting points to know:</p>
+   <h2>What are PlayOnLinux's features?--><!--#TODO#--></h2>
+   <p><!--Here is a non-exhaustive list of the interesting points to know:--><!--#TODO#--></p>
 
 	<ul>
-   <li>You don't have to own a Windows&reg; license to use PlayOnLinux.</li>
-   <li>PlayOnLinux is based on Wine, and so profits from all its features yet it keeps the user from having to deal with its complexity.</li>
-   <li>PlayOnLinux is free software.</li>
-   <li>PlayOnLinux uses Bash and Python.</li>
+   <li><!--You don't have to own a Windows&reg; license to use PlayOnLinux.-->Sie müssen keine Windows&reg; Lizenz besitzen um PlayOnLinux zu nutzen.</li>
+   <li><!--PlayOnLinux is based on Wine, and so profits from all its features yet it keeps the user from having to deal with its complexity.-->
+PlayOnLinux basiert auf Wine und profitiert von dessen Funktionen, wobei es dessen komplexität vom Benutzer fernhält.</li>
+   <li><!--PlayOnLinux is free software.-->PlayOnLinux ist eine freie Software.</li>
+   <li><!--PlayOnLinux uses Bash and Python.-->PlayOnLinux nutzt Bash und Python</li>
 	</ul>
-   <p>Nevertheless, PlayOnLinux has some bugs, as every piece of software:</p>
+   <p><!--Nevertheless, PlayOnLinux has some bugs, as every piece of software:-->Nichtsdestotrotz, PlayOnLinux hat machen Fehler, wie alle Programme</p>
 	<ul>
-   <li>Occasional performance decrease (image may be less fluid and graphics less detailed).</li>
-   <li>Not all games are supported. Nevertheless, you can use our manual installation module.</li>
+   <li><!--Occasional performance decrease (image may be less fluid and graphics less detailed).--><!--#TODO#--></li>
+   <li><!--Not all games are supported. Nevertheless, you can use our manual installation module.-->Nicht alle Spiele werden unterstützt. Für solche Fälle gibt es eine manuelle Installation.</li>
 	</ul>
 
-<h2>A few screenshots of PlayOnLinux</h2>
+<h2><!--A few screenshots of PlayOnLinux-->Ein paar Screenshots von PlayOnLinux</h2>
 
 
 <div class='pol_picture'>
 <a href='http://www.playonlinux.com/images/playonlinux_screenshots/install_wizard.png' class='lightbox'><img src="http://www.playonlinux.com/images/playonlinux_screenshots/install_wizard-min.png" alt='Miniature' /></a>
-<span class='caption'>Main window</span>
+<span class='caption'><!--Main window-->Hauptfenster</span>
 </div>
 
 <div class='pol_picture'>
 <a href='http://www.playonlinux.com/images/playonlinux_screenshots/program.png' class='lightbox'><img src="http://www.playonlinux.com/images/playonlinux_screenshots/program-min.png" alt='Miniature' /></a>
-<span class='caption'>Install window</span>
+<span class='caption'><!--Install window-->Installationsfesnter</span>
 </div>
 
 <div class='pol_picture'>
 <a href='http://www.playonlinux.com/images/playonlinux_screenshots/install.png' class='lightbox'><img src="http://www.playonlinux.com/images/playonlinux_screenshots/install-min.png" alt='Miniature' /></a>
-<span class='caption'>Install wizard</span>
+<span class='caption'><!--Install wizard-->Installationsassistent</span>
 </div>
 
 <div class='pol_picture'>
 <a href='http://www.playonlinux.com/images/playonlinux_screenshots/install2.png' class='lightbox'><img src="http://www.playonlinux.com/images/playonlinux_screenshots/install2-min.png" alt='Miniature' /></a>
-<span class='caption'>Install wizard</span>
+<span class='caption'><!--Install wizard-->Installationsassistent</span>
 </div>
 
 
 <div class='pol_picture'>
 <a href='http://www.playonlinux.com/images/playonlinux_screenshots/plugin.png' class='lightbox'><img src="http://www.playonlinux.com/images/playonlinux_screenshots/plugin-min.png" alt='Miniature' /></a>
-<span class='caption'>Plugins</span>
+<span class='caption'><!--Plugins-->Erweiterungen</span>
 </div>
 
 <div class='pol_picture'>
 <a href='http://www.playonlinux.com/images/playonlinux_screenshots/wine.png' class='lightbox'><img src="http://www.playonlinux.com/images/playonlinux_screenshots/wine-min.png" alt='Miniature' /></a>
-<span class='caption'>Wine versions</span>
+<span class='caption'><!--Wine versions-->Wine Versionen</span>
 </div>
 
 <div class="espace"></div>
 
 </div>
-<div class="presentation_item" onclick="pres_show(2)"><img src="<?php echo $url; ?>/images/design/icone2.png" alt="PlayOnLinux" /><span class="title">I'd like to download PlayOnLinux</span><span class="description">Latest version : <?php include($racine."/script_files/version2.php"); ?></span></div>
+<div class="presentation_item" onclick="pres_show(2)"><img src="<?php echo $url; ?>/images/design/icone2.png" alt="PlayOnLinux" />
+<span class="title"><!--I'd like to download PlayOnLinux-->Ich möchte PlayOnLinux herunterladen</span>
+<span class="description"><!--Latest version : -->Aktuellste Version<?php include($racine."/script_files/version2.php"); ?></span></div>
 <div id="pres_item2" class="presentation_item_hidden">
-<h2>Download PlayOnLinux</h2>
-<p>The latest version of PlayOnLinux is: <?php echo $version; ?><br />To get this version, go to the <a href='<?php echo $prefixe; ?>/download.html'>download page</a>.</p>
+<h2><!--Download PlayOnLinux-->PlayOnLinux herunterladen</h2>
+<p><!--The latest version of PlayOnLinux is: -->Die aktuellste Version von PlayOnLinux ist<?php echo $version; ?><br /><!--To get this version, go to the-->um diese Version zu erhalten, gehe auf die <a href='<?php echo $prefixe; ?>/download.html'><!--download page-->Downlaodseite</a>.</p>
 </div>
-<div class="presentation_item" onclick="pres_show(3)"><img src="<?php echo $url; ?>/images/design/icone3.png" alt="PlayOnLinux" /><span class="title">I'd like to learn how to use PlayOnLinux</span><span class="description">Where can I get help?</span></div>
+<div class="presentation_item" onclick="pres_show(3)"><img src="<?php echo $url; ?>/images/design/icone3.png" alt="PlayOnLinux" /><span class="title"><!--I'd like to learn how to use PlayOnLinux--><!--#TODO#--></span><span class="description">Where can I get help?</span></div>
 <div id="pres_item3" class="presentation_item_hidden"><h2>Learn how to use PlayOnLinux</h2>
 <p>You can read the <a href="<?php echo $page_doc; ?>">documentation</a> or ask for help on the <a href="<?php echo $prefixe; ?>/forums.html">forums</a>.</p></div>
 <div class="presentation_item" onclick="pres_show(4)" id="presentation4"><img src="<?php echo $url; ?>/images/design/icone4.png" alt="PlayOnLinux" /><span class="title">I'd like to help PlayOnLinux</span><span class="description">What can I do to help PlayOnLinux's team ?</span></div>
